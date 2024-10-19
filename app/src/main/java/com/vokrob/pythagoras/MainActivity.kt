9cb0e1555e8b4b39acfd5d3d188ac34fc3f61511
vokrob <106639488+vokrob@users.noreply.github.com>
2024-10-19 23:10:14 +0700

    Initial commit

---
+++ app/src/main/java/com/vokrob/pythagoras/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.vokrob.pythagoras
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.vokrob.pythagoras.databinding.ActivityMainBinding
+import kotlin.math.pow
+import kotlin.math.sqrt
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun onClickResult(view: View) {
+        if (!isFieldEmpty()) {
+            val result = getString(R.string.result_info) + getResult()
+            binding.tvResult.text = result
+        }
+    }
+
+    private fun isFieldEmpty(): Boolean {
+        binding.apply {
+            if (edA.text.isNullOrEmpty()) edA.error = getString(R.string.error)
+            if (edB.text.isNullOrEmpty()) edB.error = getString(R.string.error)
+
+            return edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
+    }
+
+    private fun getResult(): String {
+        val a: Double
+        val b: Double
+
+        binding.apply {
+            a = edA.text.toString().toDouble()
+            b = edB.text.toString().toDouble()
+        }
+        return sqrt((a.pow(2) + b.pow(2))).toString()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
